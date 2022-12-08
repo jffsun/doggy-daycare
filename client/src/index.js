4ed0ce103b1fedfeda13b602f09da52d598ffb9f
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2022-12-07 19:23:52 -0800

    React components and page templates; Navigation tab functionality

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,19 @@
+// Still commonly used in production react codebases 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Allows us to access DOM specific methods 
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Import bootstrap for extra styling
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// ReactDOM.render() method to renders our app component to the DOM
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // Rendering app component to the 'root' element which is the default ID created with render method
+  document.getElementById('root')
+);
