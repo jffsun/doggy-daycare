375a9a1722da6b088806f474af769a7b6c227d3d
markkoos <markusimercado@gmail.com>
2022-12-08 21:26:20 -0800

    finished resolvers and mutations on back end (for now)

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,7 +5,7 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
     Query: {
         // query to find a user
-        user: async (parent, { _id }) => {
+        me: async (parent, { _id }) => {
             // if _id doesn't exist assign params to empty object
             const params = _id ? {_id} : {};
 
@@ -16,17 +16,57 @@ const resolvers = {
     },
     Mutation: {
         // creates user, signs token, and returns both
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
+        addUser: async (parent, { firstName, lastName, email, password }) => {
+            const user = await User.create({ firstName, lastName, email, password });
 
+            // creates encrypted token for user information
+            const token = signToken(user);
+            
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            // finds user by email
+            const user = await User.findOne({ email });
+
+            if(!user) {
+                throw new AuthenticationError('No user with this email was found');
+            };
+
+            // checks that the password is correct
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Wrong password');
+            }
+
+            // creates encrypted token for user information
             const token = signToken(user);
 
             return { token, user };
         },
-        login: async (parent, { email }) => {
-            // finds user by email
-            const user = await User.findOne({ email });
+        addPet: async (parent, {_id, newPet}) => {
+            // finds user based on id then adds to newPet object to the pets array
+            const addedPet = await User.findOneAndUpdate(
+                {_id},
+                { $push: { pets: newPet }},
+                { new: true, runValidators: true},
+            );
+            
+            // returns the user with the addedpet
+            return addedPet;
+        },
+        removePet: async (parent, {_id, petId}) => {
+            // finds user based on id then removes the pet based on id from the pets array
+            const removedPet = await User.findOneAndUpdate(
+                {_id},
+                { $pull: { pets: petId }},
+                { new: true, runValidators: true},
+            );
+
+            // returns the user with the updated pets
+            return removedPet;
         }
     }
+};
 
-}
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    input PetInput {
+        name: String!
+        age: Int
+        gender: String!
+        medication: String!
+    }
+
+    type User {
+        _id: ID!
+        firstName: String!
+        lastName: String!
+        email: String!
+        pets: [Pet]
+    }
+
+    type Pet {
+        petId: ID!
+        name: String!
+        age: Int
+        gender: String!
+        medication: String!
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+        addPet(input: PetInput): User
+        removePet(petId: String!): User
+    }
+`;
+
+module.exports = typeDefs;
