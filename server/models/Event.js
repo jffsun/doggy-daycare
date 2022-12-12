c80372c4726e52fa70867bc52c6e81eacc8378e6
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2022-12-12 15:53:31 -0800

    fullCalendar routes and Event Model

---
+++ server/models/Event.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const Pet = require('./Pet');
+
+const eventSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  start: {
+    type: Date,
+    required: true,
+  },
+  end: {
+    type: Date,
+    required: true,
+  },
+  pets: [Pet.schema] 
+},
+);
+
+const Event = mongoose.model('Event', eventSchema);
+
+module.exports = Event;
