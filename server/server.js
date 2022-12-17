68050e6d445b80db7b249cdc86e686f6f8c37f51
markkoos <markusimercado@gmail.com>
2022-12-16 19:09:55 -0800

    hopefully fixed dirname

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({extended: false}));
 // recognizes request objects as a json object
 app.use(express.json())
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
   });
 
