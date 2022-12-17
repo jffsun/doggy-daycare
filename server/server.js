db9bf6e57852fa50ad4be0ff0f96854423fcf281
markkoos <markusimercado@gmail.com>
2022-12-16 19:26:44 -0800

    server files not working hahahahaha fun

--- server/server.js
+++ server/server.js
@@ -22,8 +22,12 @@ app.use(express.urlencoded({extended: false}));
 // recognizes request objects as a json object
 app.use(express.json())
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 
 const startApolloServer = async(typeDefs, resolvers) => {
